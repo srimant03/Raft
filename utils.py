d87a76fd0c4e15fe9610e840aef41c34ddb8c7d3
srimant03 <107833913+srimant03@users.noreply.github.com>
2024-03-30 18:36:39 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -20,17 +20,18 @@ def wait_for_server_startup(ip, port):
         try:
             context = zmq.Context()
             socket = context.socket(zmq.REQ)
-            socket.connect(f"tcp://{ip}:{port}")
+            socket.connect(f"tcp://localhost:{port}")
             return socket
         except Exception as e:
             traceback.print_exc(limit=1000)
 
 def send_and_recv_no_retry(msg, ip, port, timeout=-1):
-    conn = wait_for_server_startup(ip, port)
+    '''conn = wait_for_server_startup(ip, port)
+    print(port)
     resp = None
-
     try:
-        conn.sendall(msg.encode('utf-8'))
+        #conn.sendall(msg.encode('utf-8'))
+        conn.send(msg.encode('utf-8'))
         if timeout > 0:
             ready = select.select([conn], [], [], timeout)
             if ready[0]:
@@ -40,6 +41,31 @@ def send_and_recv_no_retry(msg, ip, port, timeout=-1):
 
     except Exception as e:
         traceback.print_exc(limit=1000)
+    
+    conn.close()
+    print("hi",port)
+    return resp'''
+    try:
+        conn = wait_for_server_startup(ip, port)
+    except Exception as e:
+        traceback.print_exc(limit=1000)
+        return None
+
+    print(port)
+    resp = 0
+    try:
+        conn.send(msg.encode('utf-8'))
+        if timeout > 0:
+            ready = select.select([conn], [], [], timeout)
+            if ready[0]:
+                resp = conn.recv(2048).decode('utf-8')
+        else:
+            resp = conn.recv(2048).decode('utf-8')
+
+    except Exception as e:
+        traceback.print_exc(limit=1000)
+        conn.close()
+        return None
     
     conn.close()
     return resp
